1796a82ed0f0e7c4e545cc3866da19b1e4003918
itsmee3223 <ramandaajisakaa@gmail.com>
2024-06-13 11:35:38 +0700

    value & list opreations

--- src/test/java/ramanda/ajisaka/asyraf/belajar/spring/redis/StringTest.java
+++ src/test/java/ramanda/ajisaka/asyraf/belajar/spring/redis/StringTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.time.Duration;
 
 @SpringBootTest
 public class StringTest {
@@ -14,6 +18,28 @@ public class StringTest {
     @Test
     void redisTemplate(){
         Assertions.assertNotNull(stringRedisTemplate);
+    }
+
+    @Test
+    void string() throws InterruptedException {
+        ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+        operations.set("name", "Ramanda", Duration.ofSeconds(2));
+        Assertions.assertEquals("Ramanda", operations.get("name"));
+
+        Thread.sleep(Duration.ofSeconds(3));
+        Assertions.assertNull(operations.get("name"));
+    }
+
+    @Test
+    void list() throws InterruptedException {
+        ListOperations<String, String> operations = stringRedisTemplate.opsForList();
+        operations.rightPush("names", "ramanda");
+        operations.rightPush("names", "ajisaka");
+        operations.rightPush("names", "asyraf");
+
+        Assertions.assertEquals("ramanda", operations.leftPop("names"));
+        Assertions.assertEquals("ajisaka", operations.leftPop("names"));
+        Assertions.assertEquals("asyraf", operations.leftPop("names"));
     }
 }
 
